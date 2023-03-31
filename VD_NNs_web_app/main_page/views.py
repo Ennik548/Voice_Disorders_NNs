7e0aa5b75c84f617fe764aff3192942f79cbbac9
Ennik548 <nikita.enoktaev2000@gmail.com>
2023-04-01 00:18:26 +0500

    Add main pages (test)

--- VD_NNs_web_app/main_page/views.py
+++ VD_NNs_web_app/main_page/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(reguest):
+    return render(reguest, 'main_page/index.html')
+
+
+def about(reguest):
+    return render(reguest, 'main_page/about.html')
