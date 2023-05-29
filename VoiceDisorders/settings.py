a9543058e554ce79915528c8a6552ef40e6dd608
Kiselev Vladislav <kiselev.vladislav@urfu.me>
2023-05-29 12:54:06 +0500

    Update settings.py

--- VoiceDisorders/settings.py
+++ VoiceDisorders/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'VoiceDisorders.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
+        'NAME': 'django_project_db',
         'USER': 'myprojectuser',
         'PASSWORD': 'password',
         'HOST': 'localhost',
