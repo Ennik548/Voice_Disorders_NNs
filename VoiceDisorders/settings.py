274225734b8ea06e8de7ec69935de3aa1798cced
Ennik548 <nikita.enoktaev2000@gmail.com>
2023-05-30 16:06:26 +0500

    First commit

--- VoiceDisorders/settings.py
+++ VoiceDisorders/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'VoiceDisorders.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_project_db',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        # 'NAME': 'django_project_db',
+        # 'USER': 'myprojectuser',
+        # 'PASSWORD': 'password',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
     }
 }
 
